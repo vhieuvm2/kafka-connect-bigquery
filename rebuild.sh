d78c2282178b36eca2058c9190e3939a3866b512
hieuvm2 <manhhieu1412@gmail.com>
2021-03-02 12:29:00 +0700

    rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 mvn clean package -DskipTests -Dversion=2.0.0
 mkdir -p bin/jar/ && cp kcbq-connector/target/components/packages/wepay-kafka-connect-bigquery-*/wepay-kafka-connect-bigquery-*/lib/*.jar bin/jar/
-rm -rf ../hms.rpt.cdc/.connnect-ext/wepay-kafka-connect-bigquery-2.0
-mv bin/jar ../hms.rpt.cdc/.connnect-ext/wepay-kafka-connect-bigquery-2.0
+rm -rf ../hms.data.rpt-kcn/jar
+mv bin/jar ../hms.data.rpt-kcn/
